334d528d63625c0c809040166011016a7c3852e8
Mohamed <ahdidou@novincia.com>
2021-06-29 11:44:35 +0100

    Making first api call

--- app/src/main/java/com/bam/repos/ui/repos/ReposFragment.kt
+++ app/src/main/java/com/bam/repos/ui/repos/ReposFragment.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.bam.repos.base.BaseFragment
 import com.bam.repos.databinding.FragmentReposLayoutBinding
 import com.bam.repos.model.ReposItem
+import com.google.android.material.snackbar.Snackbar
 
-class ReposFragment: BaseFragment<ReposPresenter>(), ReposView {
+class ReposFragment : BaseFragment<ReposPresenter>(), ReposView {
 
     /**
      * DataBinding instance
@@ -23,31 +25,32 @@ class ReposFragment: BaseFragment<ReposPresenter>(), ReposView {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentReposLayoutBinding.inflate(layoutInflater)
-
+        presenter.onViewCreated()
         return binding.root
     }
 
     override fun updateRepos(repos: List<ReposItem>) {
-        TODO("Not yet implemented")
+
     }
 
     override fun showError(error: String) {
-        TODO("Not yet implemented")
+        Snackbar.make(binding.reposContainer, error, Snackbar.LENGTH_LONG).setAction("Ok") {}.show()
     }
 
     override fun showLoading() {
-        TODO("Not yet implemented")
+        binding.progressBar.visibility = View.VISIBLE
     }
 
     override fun hideLoading() {
-        TODO("Not yet implemented")
-    }
-
-    override fun getContext(): Context {
-        TODO("Not yet implemented")
+        binding.progressBar.visibility = View.INVISIBLE
     }
 
     override fun instantiatePresenter(): ReposPresenter {
         return ReposPresenter(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.onViewDestroyed()
     }
 }
