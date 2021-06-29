aaa07b9f1399cfc39c57a4094cbe4737d7d17d56
Mohamed <ahdidou@novincia.com>
2021-06-29 11:34:52 +0100

    Adding Navigation Component

--- app/src/main/java/com/bam/repos/MainActivity.kt
+++ app/src/main/java/com/bam/repos/MainActivity.kt
@@ -2,10 +2,25 @@ package com.bam.repos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import com.bam.repos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        // Setting up navHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
--- app/src/main/java/com/bam/repos/ui/repos/ReposFragment.kt
+++ app/src/main/java/com/bam/repos/ui/repos/ReposFragment.kt
@@ -1,10 +1,32 @@
 package com.bam.repos.ui.repos
 
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.bam.repos.base.BaseFragment
+import com.bam.repos.databinding.FragmentReposLayoutBinding
 import com.bam.repos.model.ReposItem
 
 class ReposFragment: BaseFragment<ReposPresenter>(), ReposView {
+
+    /**
+     * DataBinding instance
+     */
+    private var _binding: FragmentReposLayoutBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentReposLayoutBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
     override fun updateRepos(repos: List<ReposItem>) {
         TODO("Not yet implemented")
     }
